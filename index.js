45e47252e9f89dab6d84ee3b49a9c2e87b74207b
devusmonjon <164606236+devusmonjon@users.noreply.github.com>
2024-09-18 10:55:47 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 // CORS sozlash - faqat http://localhost:5500 uchun
-app.use(cors({ origin: "http://localhost:5500" }));
+app.use(cors({ origin: "https://nt-react-09.vercel.app" }));
 
 // Proxy sozlash
 app.use(
